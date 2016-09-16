cd46db9b21b9ddf4c447ac87f5438b48d529df09
james sangho nah <sangho.nah@gmail.com>
2016-09-16 19:39:17 +1200

    New font

--- init.el
+++ init.el
@@ -168,7 +168,7 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
    ;; size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro"
+   dotspacemacs-default-font '("Office Code Pro"
                                :size 10
                                :weight normal
                                :width normal
